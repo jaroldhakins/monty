f5d2620af61551884a6f722dadd5d0b3b6d3f258
JessicaJulieth <3497@holbertonschool.com>
2021-09-20 23:27:54 -0500

    Add first functions

---
+++ functions.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * push_f - Add element to the stack
+ * @stack:
+ * @value: 
+ *
+ */
+void push_f(struct stack_t **stack, unsigned int value)
+{
+    stack_t * orig;
+    
+}
